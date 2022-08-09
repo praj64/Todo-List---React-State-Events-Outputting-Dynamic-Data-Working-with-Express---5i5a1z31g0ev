450e6da10e9cf3c007716c4ef8c36cd0741539ef
Prajakta Tawde <98815167+praj64@users.noreply.github.com>
2022-08-09 15:17:21 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,11 @@
 import React from "react";
-import "./../styles/App.css";
+import "../styles/App.css";
+import TodoContainer from "./TodoContainer";
 
-function App() 
-{
+function App() {
 	return (
 	<div id="main">
-	//Do not alter main div
-	//Please do not alter the functional component as tests depend on the type of component.
+		<TodoContainer/>
 	</div>
 	);
 }
